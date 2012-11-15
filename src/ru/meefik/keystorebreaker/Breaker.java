51e60ad1df517bff37514e1de5013942f05dae27
Anton Skshidlevsky <meefik@gmail.com>
2012-11-16 01:28:45 +0400

    1.0
    Change-Id: I06cda8acb0aaf86d8416ed176b085a5380cb06f9

--- src/ru/meefik/keystorebreaker/Breaker.java
+++ src/ru/meefik/keystorebreaker/Breaker.java
@@ -5,11 +5,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
- 
+
 public class Breaker extends Thread {
-	
+
 	private ByteArrayInputStream buf;
-	//private char[] char_seq = "0123456789abcdefjhijklmnopqrstuvwxyz".toCharArray();
+	// private char[] char_seq =
+	// "0123456789abcdefjhijklmnopqrstuvwxyz".toCharArray();
 	private char[] char_seq;
 	private int seq_len;
 	private int[] int_seq;
@@ -19,109 +20,111 @@ public class Breaker extends Thread {
 	private boolean alive = false;
 	public final char[] firstPwd;
 	public final char[] lastPwd;
-	
-	public Breaker(String ksFile, char[] seq, char[] fPwd, char[] lPwd, int threads) {
-        try {
-            File file = new File(ksFile);
-            FileInputStream fis = new FileInputStream(file);
-            byte[] fileBytes = new byte[(int)file.length()];
-            fis.read(fileBytes);
-            buf = new ByteArrayInputStream(fileBytes);
-            fis.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-        // normalize
-        char[] nPwd = new char[lPwd.length];
-    	for (int i = 0; i < lPwd.length; i++) {
-    		if (i < fPwd.length)
-    			nPwd[i] = fPwd[i];
-    		else
-    			nPwd[i] = seq[0];
-    	}
 
-    	this.firstPwd = nPwd;
-        this.lastPwd = lPwd;    	
-        this.passphrase = this.firstPwd;
-        this.char_seq = seq;
-        this.seq_len = char_seq.length;
-        setIntSeq(this.firstPwd);
-        //System.out.println(getPwd());
+	public Breaker(String ksFile, char[] seq, char[] fPwd, char[] lPwd,
+			int threads) {
+		try {
+			File file = new File(ksFile);
+			FileInputStream fis = new FileInputStream(file);
+			byte[] fileBytes = new byte[(int) file.length()];
+			fis.read(fileBytes);
+			buf = new ByteArrayInputStream(fileBytes);
+			fis.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// normalize
+		char[] nPwd = new char[lPwd.length];
+		for (int i = 0; i < lPwd.length; i++) {
+			if (i < fPwd.length)
+				nPwd[i] = fPwd[i];
+			else
+				nPwd[i] = seq[0];
+		}
+
+		this.firstPwd = nPwd;
+		this.lastPwd = lPwd;
+		this.passphrase = this.firstPwd;
+		this.char_seq = seq;
+		this.seq_len = char_seq.length;
+		setIntSeq(this.firstPwd);
+		// System.out.println(getPwd());
 	}
-	
-    public char[] getPasswd() {
-    	return passphrase;
-    }
-    
-    public boolean isFound() {
-    	return found;
-    }
-    
-    public boolean isActive() {
-    	return alive;
-    }
-    
-    public void inactive() {
-    	alive = false;
-    }
-    
-    private void setIntSeq(char[] pwd) {
-    	depth = pwd.length;
-    	int_seq = new int[depth];
-    	for (int i = 0; i < depth; i++) {
-    		for (int j = 0; j < seq_len; j++) {
-    			if (char_seq[j] == pwd[i]) {
-    	    		int_seq[i] = j;
-    			}
-    		}
-    	}
-    }
-    
-    private boolean nextIntSeq() {
-    	for (int i = 0; i < depth; i++) {
-    		if (int_seq[i] < seq_len-1) {
-     			int_seq[i]++;
-     			return true;
-     		} else {
-     			int_seq[i] = 0;
-     		}
-    	}
-    	return false;
-    }
-    
-    private char[] getPwd() {
-    	char[] pwd = new char[depth];
-    	for (int i = 0; i < depth; i++) {
-    		pwd[i] = char_seq[int_seq[i]];
-    	}
-    	return pwd;
-    }
+
+	public char[] getPasswd() {
+		return passphrase;
+	}
+
+	public boolean isFound() {
+		return found;
+	}
+
+	public boolean isActive() {
+		return alive;
+	}
+
+	public void inactive() {
+		alive = false;
+	}
+
+	private void setIntSeq(char[] pwd) {
+		depth = pwd.length;
+		int_seq = new int[depth];
+		for (int i = 0; i < depth; i++) {
+			for (int j = 0; j < seq_len; j++) {
+				if (char_seq[j] == pwd[i]) {
+					int_seq[i] = j;
+				}
+			}
+		}
+	}
+
+	private boolean nextIntSeq() {
+		for (int i = 0; i < depth; i++) {
+			if (int_seq[i] < seq_len - 1) {
+				int_seq[i]++;
+				return true;
+			} else {
+				int_seq[i] = 0;
+			}
+		}
+		return false;
+	}
+
+	private char[] getPwd() {
+		char[] pwd = new char[depth];
+		for (int i = 0; i < depth; i++) {
+			pwd[i] = char_seq[int_seq[i]];
+		}
+		return pwd;
+	}
 
 	@Override
 	public void run() {
 		alive = true;
-        KeyStore ks = null;
-        try {
-            ks = KeyStore.getInstance(KeyStore.getDefaultType());
-        } catch (KeyStoreException e) {
-            e.printStackTrace();
-        }
-        while(alive) {
-        	buf.reset();
-            try {
-            	//System.out.println(getPwd());
-                if (!nextIntSeq()) break;
-                passphrase = getPwd();
-                //if (Arrays.equals(passphrase,lastPwd)) alive = false;
-                ks.load(buf, passphrase);
-                found = true;
-                alive = false;
-            } catch (Throwable t) {
-                continue;
-            }
-        }
-        alive = false;
+		KeyStore ks = null;
+		try {
+			ks = KeyStore.getInstance(KeyStore.getDefaultType());
+		} catch (KeyStoreException e) {
+			e.printStackTrace();
+		}
+		while (alive) {
+			buf.reset();
+			try {
+				// System.out.println(getPwd());
+				if (!nextIntSeq())
+					break;
+				passphrase = getPwd();
+				// if (Arrays.equals(passphrase,lastPwd)) alive = false;
+				ks.load(buf, passphrase);
+				found = true;
+				alive = false;
+			} catch (Throwable t) {
+				continue;
+			}
+		}
+		alive = false;
 	}
 
 }
