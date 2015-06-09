5263207da5aa8b2bdc735395a6586e8ece7291c4
Anton Skshidlevsky <meefik@gmail.com>
2015-06-09 12:11:42 +0300

    Code formatting

--- src/ru/meefik/keystorebreaker/Breaker.java
+++ src/ru/meefik/keystorebreaker/Breaker.java
@@ -10,14 +10,14 @@ public class Breaker extends Thread {
     private static final int DIGEST_LENGTH = 20;
 
     private final char[] char_seq;
-	private final int seq_len;
-	private int[] int_seq;
-	private int depth;
-	private char[] passphrase;
-	private boolean found = false;
-	private boolean alive = false;
-	public final char[] firstPwd;
-	public final char[] lastPwd;
+    private final int seq_len;
+    private int[] int_seq;
+    private int depth;
+    private char[] passphrase;
+    private boolean found = false;
+    private boolean alive = false;
+    public final char[] firstPwd;
+    public final char[] lastPwd;
     private final byte[] keystore;
     private final byte[] signature;
     private final byte[] salt;
@@ -29,13 +29,11 @@ public class Breaker extends Thread {
         this.keystore = new byte[(int) file.length() - DIGEST_LENGTH];
         this.signature = new byte[DIGEST_LENGTH];
 
-        try {
+        try (DataInputStream stream = new DataInputStream(new FileInputStream(file))) {
             this.md = MessageDigest.getInstance("SHA");
             this.salt = "Mighty Aphrodite".getBytes("UTF8");
-            final DataInputStream stream = new DataInputStream(new FileInputStream(file));
             stream.readFully(keystore);
             stream.readFully(signature);
-            stream.close();
         } catch (UnsupportedEncodingException e) {
             throw new IllegalStateException("Unable to initialize salt: " + e.getMessage(), e);
         } catch (IOException e) {
@@ -44,79 +42,79 @@ public class Breaker extends Thread {
             throw new IllegalStateException("Unable to initialize MessageDigest: " + e.getMessage(), e);
         }
 
-		// normalize
-		char[] nPwd = new char[lPwd.length];
-		for (int i = 0; i < lPwd.length; i++) {
-			if (i < fPwd.length)
-				nPwd[i] = fPwd[i];
-			else
-				nPwd[i] = seq[0];
-		}
+        // normalize
+        char[] nPwd = new char[lPwd.length];
+        for (int i = 0; i < lPwd.length; i++) {
+            if (i < fPwd.length) {
+                nPwd[i] = fPwd[i];
+            } else {
+                nPwd[i] = seq[0];
+            }
+        }
 
-		this.firstPwd = nPwd;
-		this.lastPwd = lPwd;
-		this.passphrase = this.firstPwd;
-		this.char_seq = seq;
-		this.seq_len = char_seq.length;
-		setIntSeq(this.firstPwd);
-		// System.out.println(getPwd());
-	}
+        this.firstPwd = nPwd;
+        this.lastPwd = lPwd;
+        this.passphrase = this.firstPwd;
+        this.char_seq = seq;
+        this.seq_len = char_seq.length;
+        setIntSeq(this.firstPwd);
+    }
 
-	public char[] getPasswd() {
-		return passphrase;
-	}
+    public char[] getPasswd() {
+        return passphrase;
+    }
 
-	public boolean isFound() {
-		return found;
-	}
+    public boolean isFound() {
+        return found;
+    }
 
-	public boolean isActive() {
-		return alive;
-	}
+    public boolean isActive() {
+        return alive;
+    }
 
-	public void inactive() {
-		alive = false;
-	}
+    public void inactive() {
+        alive = false;
+    }
 
-	private void setIntSeq(char[] pwd) {
-		depth = pwd.length;
-		int_seq = new int[depth];
-		for (int i = 0; i < depth; i++) {
-			for (int j = 0; j < seq_len; j++) {
-				if (char_seq[j] == pwd[i]) {
-					int_seq[i] = j;
-				}
-			}
-		}
-	}
+    private void setIntSeq(char[] pwd) {
+        depth = pwd.length;
+        int_seq = new int[depth];
+        for (int i = 0; i < depth; i++) {
+            for (int j = 0; j < seq_len; j++) {
+                if (char_seq[j] == pwd[i]) {
+                    int_seq[i] = j;
+                }
+            }
+        }
+    }
 
-	private boolean nextIntSeq() {
-		for (int i = 0; i < depth; i++) {
-			if (int_seq[i] < seq_len - 1) {
-				int_seq[i]++;
-				return true;
-			} else {
-				int_seq[i] = 0;
-			}
-		}
-		return false;
-	}
+    private boolean nextIntSeq() {
+        for (int i = 0; i < depth; i++) {
+            if (int_seq[i] < seq_len - 1) {
+                int_seq[i]++;
+                return true;
+            } else {
+                int_seq[i] = 0;
+            }
+        }
+        return false;
+    }
 
-	private char[] getPwd() {
-		char[] pwd = new char[depth];
-		for (int i = 0; i < depth; i++) {
-			pwd[i] = char_seq[int_seq[i]];
-		}
-		return pwd;
-	}
+    private char[] getPwd() {
+        char[] pwd = new char[depth];
+        for (int i = 0; i < depth; i++) {
+            pwd[i] = char_seq[int_seq[i]];
+        }
+        return pwd;
+    }
 
-	@Override
-	public void run() {
-		alive = true;
+    @Override
+    public void run() {
+        alive = true;
         while (alive) {
-            // System.out.println(getPwd());
-            if (!nextIntSeq())
+            if (!nextIntSeq()) {
                 break;
+            }
             passphrase = getPwd();
 
             final MessageDigest digest = getPreKeyedHash(passphrase);
@@ -130,7 +128,7 @@ public class Breaker extends Thread {
             alive = false;
         }
         alive = false;
-	}
+    }
 
     /**
      * Generate a new SHA message digest, based on the password
--- src/ru/meefik/keystorebreaker/Main.java
+++ src/ru/meefik/keystorebreaker/Main.java
@@ -4,36 +4,36 @@ import java.io.File;
 
 public class Main {
 
-	public static void main(String[] args) {
-		if (args.length != 5) {
-			System.out
-					.println("Usage: java -jar KeystoreBreaker.jar <keystore file> <sequence> <first passwd> <last passwd> <number of threads>");
-			return;
-		}
-		String ksFile = "test.jks";
-		char[] char_seq = "0123456789abcdefghijklmnopqrstuvwxyz".toCharArray();
-		char[] firstPwd = "1234".toCharArray();
-		char[] lastPwd = "zzzz".toCharArray();
-		int threads = 1;
+    public static void main(String[] args) {
+        if (args.length != 5) {
+            System.out.println("Usage: java -jar KeystoreBreaker.jar <keystore file> <sequence> <first passwd> <last passwd> <number of threads>");
+            return;
+        }
+        
+        String ksFile = "test.jks";
+        char[] char_seq = "0123456789abcdefghijklmnopqrstuvwxyz".toCharArray();
+        char[] firstPwd = "1234".toCharArray();
+        char[] lastPwd = "zzzz".toCharArray();
+        int threads = 1;
 
-		try {
-			ksFile = args[0];
-			char_seq = args[1].toCharArray();
-			firstPwd = args[2].toCharArray();
-			lastPwd = args[3].toCharArray();
-			threads = Integer.parseInt(args[4]);
-		} catch (Throwable t) {
-			System.out.println("Error parse input parameters!");
-			return;
-		}
+        try {
+            ksFile = args[0];
+            char_seq = args[1].toCharArray();
+            firstPwd = args[2].toCharArray();
+            lastPwd = args[3].toCharArray();
+            threads = Integer.parseInt(args[4]);
+        } catch (Throwable t) {
+            System.out.println("Error parse input parameters!");
+            return;
+        }
 
-		File f = new File(ksFile);
-		if (!f.exists()) {
-			System.out.println("Keystore file not found!");
-			return;
-		}
+        File f = new File(ksFile);
+        if (!f.exists()) {
+            System.out.println("Keystore file not found!");
+            return;
+        }
 
-		new Manager(ksFile, char_seq, firstPwd, lastPwd, threads);
-	}
+        new Manager(ksFile, char_seq, firstPwd, lastPwd, threads);
+    }
 
 }
